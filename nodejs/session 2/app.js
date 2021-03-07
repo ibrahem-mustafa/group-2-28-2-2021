ccb70d4e1dd7ae07e000e7280562da530762e5dd
ibrahem mustafa <ib.aldomyate@gmail.com>
2021-03-07 11:03:28 +0200

    session 5 part 1

---
+++ nodejs/session 2/app.js
@@ -0,0 +1,68 @@
+// receive request from client
+// get the request url
+// send html file based on request url
+
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+const app = http.createServer((req, res) => {
+  // const url = req.url;
+  // console.log(url)
+
+  //   switch (url) {
+  //     case "/":
+  //     case "/index":
+  //       sendFile("index.html", res);
+  //       break;
+  //     case "/about":
+  //       sendFile("about.html", res);
+  //       break;
+  //     case "/main.js":
+  //       sendFile("main.js", res);
+  //       break;
+  //     default:
+  //       sendFile("404.html", res);
+  //       break;
+  //   }
+
+  const reqUrl = url.parse(req.url, true);
+  switch (reqUrl.pathname) {
+    case "/":
+    case "/index":
+      sendFile("index.html", res);
+      break;
+    case "/about":
+      sendFile("about.html", res);
+      break;
+    case "/main.js":
+      sendFile("main.js", res);
+      break;
+    case "/todo-manager":
+      sendFile("taskManager/index.html", res);
+      break;
+    case "/app.js":
+      sendFile("taskManager/app.js", res);
+      break;
+    default:
+      sendFile("404.html", res);
+      break;
+  }
+
+//   res.end("Test");
+});
+
+function sendFile(filaPath, res) {
+  console.log("Request Of File: " + filaPath);
+  try {
+    const file = fs.readFileSync(filaPath);
+    res.end(file);
+  } catch (err) {
+    console.log(err);
+    res.end("Server Has Error");
+  }
+}
+
+app.on("listening", () => console.log("Server Running On Port 3000"));
+
+app.listen(3000);
---
+++ nodejs/session 2/taskManager/app.js
@@ -0,0 +1,75 @@
+// fetch data
+// create element
+// render data
+
+const todoListContainer = document.querySelector(".todoListContainer");
+
+
+let todoList = todoListContainer.querySelector(".todoList");
+
+if (!todoList) {
+    createElement('ul', {
+        class: 'todoList',
+        id: 'list'
+    }, null, (ul) => {
+        todoListContainer.appendChild(ul)
+        todoList = ul;
+    })
+}
+
+
+
+(async function fetchTodos() {
+  const response = await fetch("http://jsonplaceholder.typicode.com/todos/");
+  const todosData = await response.json();
+  todosData.slice(0, 10).forEach((todo) => renderTodo(todoList, todo));
+})();
+
+
+const todoForm = document.querySelector('.todoForm')
+const input = todoForm.querySelector('.formInput');
+const btn = todoForm.querySelector('.btn')
+
+
+btn.addEventListener('click', () => {
+    const todo = {
+        id: new Date().getTime(),
+        title: input.value
+    }
+
+    renderTodo(todoList, todo)
+
+    input.value = '';
+
+})
+
+function renderTodo(list, todo) {
+
+    createElement('li', {class: 'todo', id: todo.id}, todo.title, (li) => {
+
+        createElement('button', {class: 'btn'}, 'X', (button) => {
+            
+            button.addEventListener('click', () => {
+                list.removeChild(li)
+            })
+
+            li.appendChild(button)
+            list.appendChild(li)
+
+        })
+    })
+}
+
+function createElement(tag, attributes, content, afterCreate) {
+    const ele = document.createElement(tag);
+
+    const attributesKeys = Object.keys(attributes)
+    attributesKeys.forEach(key => {
+        ele.setAttribute(key, attributes[key])
+    })
+
+    if (content) {
+        ele.textContent = content;
+    }
+    afterCreate(ele)
+}
