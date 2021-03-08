bdcda8f5b68f402373d0eae418a7d0f310f9c763
ibrahem mustafa <ib.aldomyate@gmail.com>
2021-03-08 11:45:42 +0200

    session 7 part 1

--- express/blog/app.js
+++ express/blog/app.js
@@ -9,24 +9,7 @@ const data = require('./playground/sum')
 
 const app = express();
 
-app.get('/', (req, res) => {
-  
-  const result = data.sum(10, 40)
-  // res.send('Hello')
-  res.json( { result, results: data.results } )
-})
 
-app.post('/create', (req, res) => {
-  res.json({msg: 'POST Request'})
-})
-
-app.put('/update', (req, res) => {
-  res.json({msg: 'Put Request'})
-})
-
-app.delete('/delete', (req, res) => {
-  res.json({msg: 'Delete Request'})
-})
 
 
 app.use(logger('dev'));
@@ -34,6 +17,48 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+// (REQUEST) => [HEAD, BODY] => HEAD (INFORMATION ABOUT REQUEST) | BODY (REQUEST DATA)
+
+const articles = [];
+
+app.get("/", (req, res) => {
+  // res.send('Hello')
+  res.json({
+    articles
+  });
+});
+
+app.post('/create', (req, res) => {
+
+  const data = req.body;
+
+  articles.push({
+    id: articles.length + 1,
+    title: data.title,
+    content: data.content
+  })
+
+  res.json({articles})
+})
+
+app.put('/update', (req, res) => {
+
+  const {id, title, content} = req.body;
+
+  console.log(id)
+  console.log(title)
+  console.log(content)
+
+  // console.log(id, title, content)
+
+  res.json({msg: 'Put Request'})
+})
+
+app.delete('/delete', (req, res) => {
+  res.json({msg: 'Delete Request'})
+})
 
 
 // catch 404 and forward to error handler
