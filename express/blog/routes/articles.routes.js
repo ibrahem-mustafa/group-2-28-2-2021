8610b247fd6b3b5852f5be1ae681431cf540c436
ibrahem mustafa <ib.aldomyate@gmail.com>
2021-03-11 11:22:13 +0200

    session 10 part 1

--- express/blog/routes/articles.routes.js
+++ express/blog/routes/articles.routes.js
@@ -5,23 +5,20 @@ const { Article } = require("../models/article.model");
 const { User } = require("../models/user.model");
 const jwt  = require('jsonwebtoken')
 const {JWT_SECRET} = require('../config/secrets')
+const {ValidateToken} = require('../middleware/validation/validateToken.middleware')
+const {
+  IsArticlePublisher,
+} = require("../middleware/articles/isArticlePublisher.middleware");
 
-router.get("/", async (req, res) => {
+router.get("/",ValidateToken, async (req, res) => {
   // res.send('Hello')
-  try {
-    
-    const token = req.headers.authorization.split(" ")[1];
+  
+  const articles = await Article.find({'publisher.id': req.user.id});
 
-    const user = jwt.verify(token, JWT_SECRET);
-    console.log(user);
-    const articles = await Article.find({'publisher.id': user.id});
-
-    res.json({
-      articles,
-    });
-  } catch(err) {
-    res.status(400).json({msg: 'Invalid User Token'})
-  }
+  res.json({
+    articles,
+  });
+  
 });
 
 // router.get("/:id", (req, res) => {
@@ -35,15 +32,11 @@ router.get("/", async (req, res) => {
 //   });
 // });
 
-router.post("/", async (req, res) => {
-  // TODO: 1: remove publisherId from the body
-  // TODO: 2: get token from headers
-  // TODO: 3: verify user token
-  // TODO: 4: create article with user data
+router.post("/", ValidateToken , async (req, res) => {
+ 
+  const { title, content } = req.body;
 
-  const { title, content, publisherId } = req.body;
-
-  const user = await User.findById(publisherId);
+  const user = req.user;
 
   const article = new Article({
     title,
@@ -59,22 +52,12 @@ router.post("/", async (req, res) => {
   res.json({ article });
 });
 
-router.put("/:id", async (req, res) => {
-  // TODO: 1: get token from headers
-  // TODO: 2: verify user token
-  // TODO: 3: update article
-
+router.put("/:id", ValidateToken, IsArticlePublisher, async (req, res) => {
+  
   const { id } = req.params;
   const { title, content } = req.body;
 
-  //   get article that math given id
-  //     const article = await Article.findById(id)
-
-  //     // update article title & content
-  //     article.title = title;
-  //     article.content = content;
-  // //
-  //     await article.save()
+  console.log(req.article)
 
   const article = await Article.findByIdAndUpdate(
     id,
@@ -86,23 +69,89 @@ router.put("/:id", async (req, res) => {
       new: true,
     }
   );
+
+  if (!article) {
+    return res.status(404).json({err: 'Article Not Found'})
+  }
   // send response with updated article
   res.json({ article });
 });
 
-router.delete("/:id", async (req, res) => {
-  // TODO: 1: get token from headers
-  // TODO: 2: verify user token
-  // TODO: 3: delete article
-
+router.delete("/:id", ValidateToken, IsArticlePublisher, async (req, res) => {
   const { id } = req.params;
-  //   // get index of article that matches given id
-  //   const articleIndex = articles.findIndex((a) => a.id == id);
-  //   // remove the article with that index from articles array;
-  //   articles.splice(articleIndex, 1);
-  //   // return response with the remaining article
   await Article.findByIdAndDelete(id);
   res.json({ msg: "Article Deleted Successfully" });
 });
+
+
+// router.post("/", async (req, res) => {
+//   // TODO: 1: remove publisherId from the body
+//   // TODO: 2: get token from headers
+//   // TODO: 3: verify user token
+//   // TODO: 4: create article with user data
+
+//   const { title, content, publisherId } = req.body;
+
+//   const user = await User.findById(publisherId);
+
+//   const article = new Article({
+//     title,
+//     content,
+//     publisher: {
+//       id: user.id,
+//       name: user.name,
+//     },
+//   });
+
+//   await article.save();
+
+//   res.json({ article });
+// });
+
+// router.put("/:id", async (req, res) => {
+//   // TODO: 1: get token from headers
+//   // TODO: 2: verify user token
+//   // TODO: 3: update article
+
+//   const { id } = req.params;
+//   const { title, content } = req.body;
+
+//   //   get article that math given id
+//   //     const article = await Article.findById(id)
+
+//   //     // update article title & content
+//   //     article.title = title;
+//   //     article.content = content;
+//   // //
+//   //     await article.save()
+
+//   const article = await Article.findByIdAndUpdate(
+//     id,
+//     {
+//       title,
+//       content,
+//     },
+//     {
+//       new: true,
+//     }
+//   );
+//   // send response with updated article
+//   res.json({ article });
+// });
+
+// router.delete("/:id", async (req, res) => {
+//   // TODO: 1: get token from headers
+//   // TODO: 2: verify user token
+//   // TODO: 3: delete article
+
+//   const { id } = req.params;
+//   //   // get index of article that matches given id
+//   //   const articleIndex = articles.findIndex((a) => a.id == id);
+//   //   // remove the article with that index from articles array;
+//   //   articles.splice(articleIndex, 1);
+//   //   // return response with the remaining article
+//   await Article.findByIdAndDelete(id);
+//   res.json({ msg: "Article Deleted Successfully" });
+// });
 
 module.exports = router;
